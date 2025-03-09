e5f1cc227feb5529085b2fae7463631ac91473d0
ocean386 <Tomas@20220808>
2025-03-09 10:51:26 +0800

    初始化项目-定时任务采集金融数据

---
+++ core.go
@@ -0,0 +1,53 @@
+//	stocktask
+//
+//	Description: stocktask service
+//
+//	Schemes: http, https
+//	Host: localhost:9100
+//	BasePath: /
+//	Version: 0.0.1
+//	SecurityDefinitions:
+//	  Token:
+//	    type: apiKey
+//	    name: Authorization
+//	    in: header
+//	Security:
+//	  Token:
+//	Consumes:
+//	  - application/json
+//
+//	Produces:
+//	  - application/json
+//
+// swagger:meta
+package main
+
+import (
+	"flag"
+	"fmt"
+
+	"github.com/ocean386/stock-task/internal/config"
+	"github.com/ocean386/stock-task/internal/handler"
+	"github.com/ocean386/stock-task/internal/svc"
+
+	"github.com/zeromicro/go-zero/core/conf"
+	"github.com/zeromicro/go-zero/rest"
+)
+
+var configFile = flag.String("f", "etc/core.yaml", "the config file")
+
+func main() {
+	flag.Parse()
+
+	var c config.Config
+	conf.MustLoad(*configFile, &c, conf.UseEnv())
+
+	server := rest.MustNewServer(c.RestConf, rest.WithCors("*"))
+	defer server.Stop()
+
+	ctx := svc.NewServiceContext(c)
+	handler.RegisterHandlers(server, ctx)
+
+	fmt.Printf("Starting server at %s:%d...\n", c.Host, c.Port)
+	server.Start()
+}
---
+++ internal/svc/service_context.go
@@ -0,0 +1,16 @@
+package svc
+
+import (
+	"github.com/ocean386/stock-task/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+
+	return &ServiceContext{
+		Config: c,
+	}
+}
